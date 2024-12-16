adfbb81e1c99da8e9f508d6aa35369215c940210
azurwastaken <caseaux.maxime@gmail.com>
2024-12-16 18:01:50 +0100

    fix some stuff + test

--- src/synchroniser/feeder.rs
+++ src/synchroniser/feeder.rs
@@ -10,7 +10,7 @@ use tokio::sync::{broadcast, mpsc};
 use crate::{
     db::{Column, Db},
     types::{
-        batch::Batch, block_header::BlockHeader, certificate::Certificate, network::{NetworkRequest, ReceivedObject, RequestPayload, SyncData, SyncRequest, SyncResponse}, traits::{AsHex, Hash}, transaction::Transaction, Digest, RequestId
+        batch::Batch, block_header::BlockHeader, certificate::Certificate, network::{NetworkRequest, ReceivedObject, RequestPayload, SyncData, SyncRequest, SyncResponse}, traits::{AsHex, Hash}, transaction::Transaction, RequestId
     },
 };
 
@@ -59,12 +59,12 @@ impl Feeder {
                         SyncRequest::RequestBatches(payload) => {
                             self.try_retrieve_data::<Batch<Transaction>>(&payload, request_id, request.sender, Column::Batches).await?
                         }
-                        SyncRequest::SyncDigest(_) => todo!(),
+                        _ => {},
                     }
                 },
                 Err(e) => tracing::error!("Feeder: Failed to recv {}",e),
             }
-        } 
+        }
     }
 
     pub async fn try_retrieve_data<T>(
@@ -93,7 +93,7 @@ impl Feeder {
             len if len == certif_to_retrieve => {
                 SyncResponse::Success(req_id.into(), datas.into_sync_data())
             }
-            len if len < certif_to_retrieve => {
+            len if len != 0 && len < certif_to_retrieve => {
                 SyncResponse::Partial(req_id.into(), datas.into_sync_data())
             }
             len if len == 0 => SyncResponse::Failure,
@@ -113,12 +113,167 @@ impl Feeder {
 
 #[cfg(test)]
 pub mod test{
+    use std::sync::Arc;
+    use crate::types::{network::{RequestPayload, SyncData, SyncResponse}, traits::{AsHex, Hash}, Digest};
+    use libp2p::PeerId;
+    use tokio::sync::{broadcast, mpsc};
+    use tokio_util::sync::CancellationToken;
+
+    use crate::{db::Db, synchroniser::feeder::Feeder, types::{batch::Batch, network::{NetworkRequest, ReceivedObject, SyncRequest}, traits::Random, transaction::Transaction}};
 
     #[rstest::rstest]
     #[tokio::test]
-    async fn basic_test() {
+    async fn feeder_full_recovery() {
+        //create db
+        tracing::info!("Starting test");
+        let db = Arc::new(Db::new("/tmp/feeder_test_db_1".into()).expect("failed to open db"));
         //feed db
+        let batches: Vec<Batch<Transaction>> = (0..10).map(|_| Batch::<Transaction>::random(30)).collect();
+        let batch_ids: Vec<Digest> = batches.iter()
+            .map(|elm: &Batch<Transaction>| elm.digest().into())
+            .collect();
+
+        // Insert data in database
+        db.insert(crate::db::Column::Batches, &batch_ids[0].as_hex_string(), batches[0].clone()).unwrap();
+        let result= db.get::<Batch<Transaction>>(crate::db::Column::Batches, &batch_ids[0].as_hex_string()).unwrap().unwrap();
+        assert_eq!(result, batches[0]);
+
+        //run the feeder
+        let cancellation_token = CancellationToken::new();
+        let (sync_req_tx, sync_req_rx) = broadcast::channel::<ReceivedObject<SyncRequest>>(10_000);
+        let (network_tx, mut network_rx) = mpsc::channel::<NetworkRequest>(10_000);
+        let feeder_handle = Feeder::spawn(
+            cancellation_token.clone(),
+            sync_req_rx,
+            network_tx.clone(),
+            db.clone(),
+        );
+
         //mock a message in canal
-        //verify
+        let peer_id = PeerId::random();
+        let sync_request = SyncRequest::RequestBatches(vec![batch_ids[0]]);
+        let received_object = ReceivedObject{ object: sync_request, sender: peer_id};
+        let _ = sync_req_tx.send(received_object);
+
+
+        if let Some(msg) = network_rx.recv().await {
+            if let NetworkRequest::SendTo(pid, request_payload) = msg {
+                assert_eq!(peer_id,pid);
+                if let RequestPayload::SyncResponse(SyncResponse::Success(_reqid, syncdata)) = request_payload {
+                    if let SyncData::Batches(batch_resp) = syncdata {
+                        assert_eq!(batch_resp[0],batches[0]);
+                    } else {
+                        panic!();
+                    }
+                } else {
+                    panic!();
+                }
+            } else {
+                panic!();
+            }
+        }
+        cancellation_token.cancel();
+        let _res = tokio::try_join!(feeder_handle);
+    }
+
+    #[rstest::rstest]
+    #[tokio::test]
+    async fn feeder_partial_recovery() {
+        //create db
+        tracing::info!("Starting test");
+        let db = Arc::new(Db::new("/tmp/feeder_test_db_2".into()).expect("failed to open db"));
+        //feed db
+        let batches: Vec<Batch<Transaction>> = (0..10).map(|_| Batch::<Transaction>::random(30)).collect();
+        let batch_ids: Vec<Digest> = batches.iter()
+            .map(|elm: &Batch<Transaction>| elm.digest().into())
+            .collect();
+
+        // Insert data in database
+        db.insert(crate::db::Column::Batches, &batch_ids[0].as_hex_string(), batches[0].clone()).unwrap();
+        let result= db.get::<Batch<Transaction>>(crate::db::Column::Batches, &batch_ids[0].as_hex_string()).unwrap().unwrap();
+        assert_eq!(result, batches[0]);
+
+        //run the feeder
+        let cancellation_token = CancellationToken::new();
+        let (sync_req_tx, sync_req_rx) = broadcast::channel::<ReceivedObject<SyncRequest>>(10_000);
+        let (network_tx, mut network_rx) = mpsc::channel::<NetworkRequest>(10_000);
+        let feeder_handle = Feeder::spawn(
+            cancellation_token.clone(),
+            sync_req_rx,
+            network_tx.clone(),
+            db.clone(),
+        );
+
+        //mock a message in canal
+        let peer_id = PeerId::random();
+        let sync_request = SyncRequest::RequestBatches(batch_ids);
+        let received_object = ReceivedObject{ object: sync_request, sender: peer_id};
+        let _ = sync_req_tx.send(received_object);
+
+        
+        if let Some(msg) = network_rx.recv().await {
+            if let NetworkRequest::SendTo(pid, request_payload) = msg {
+                assert_eq!(peer_id,pid);
+                if let RequestPayload::SyncResponse(SyncResponse::Partial(_reqid, syncdata)) = request_payload {
+                    if let SyncData::Batches(batch_resp) = syncdata {
+                        assert_eq!(batch_resp[0],batches[0]);
+                    } else {
+                        panic!();
+                    }
+                } else {
+                    panic!();
+                }
+            } else {
+                panic!();
+            }
+        }
+        cancellation_token.cancel();
+        let _res = tokio::try_join!(feeder_handle);
+    }
+
+    #[rstest::rstest]
+    #[tokio::test]
+    async fn feeder_failed_recovery() {
+        //create db
+        tracing::info!("Starting test");
+        let db = Arc::new(Db::new("/tmp/feeder_test_db_3".into()).expect("failed to open db"));
+        //feed db
+        let batches: Vec<Batch<Transaction>> = (0..10).map(|_| Batch::<Transaction>::random(30)).collect();
+        let batch_ids: Vec<Digest> = batches.iter()
+            .map(|elm: &Batch<Transaction>| elm.digest().into())
+            .collect();
+
+        //run the feeder
+        let cancellation_token = CancellationToken::new();
+        let (sync_req_tx, sync_req_rx) = broadcast::channel::<ReceivedObject<SyncRequest>>(10_000);
+        let (network_tx, mut network_rx) = mpsc::channel::<NetworkRequest>(10_000);
+        let feeder_handle = Feeder::spawn(
+            cancellation_token.clone(),
+            sync_req_rx,
+            network_tx.clone(),
+            db.clone(),
+        );
+
+        //mock a message in canal
+        let peer_id = PeerId::random();
+        let sync_request = SyncRequest::RequestBatches(vec![batch_ids[0]]);
+        let received_object = ReceivedObject{ object: sync_request, sender: peer_id};
+        let _ = sync_req_tx.send(received_object);
+
+        
+        if let Some(msg) = network_rx.recv().await {
+            if let NetworkRequest::SendTo(pid, request_payload) = msg {
+                assert_eq!(peer_id,pid);
+                if let RequestPayload::SyncResponse(sync_response) = request_payload {
+                    assert_eq!(sync_response, SyncResponse::Failure);
+                } else {
+                    panic!();
+                }
+            } else {
+                panic!();
+            }
+        }
+        cancellation_token.cancel();
+        let _res = tokio::try_join!(feeder_handle);
     }
 }
